d6ee986628ab8518d38800de5cefd8c47e53c5e6
DJ Viking <anders.swedishviking@gmail.com>
2020-10-19 21:54:55 -0400

    21.3.5 trying to get the console log of all the thoughts on the front end from the backend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHTS } from '../utils/queries.js';
 
 const Home = () => {
+  //use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];//optional chaining supported by new browsers as of oct 19 2020 but not node yet we will see later.
+  console.log(thoughts);
   return (
     <main>
       <div className='flex-row justify-space-between'>
@@ -11,3 +17,12 @@ const Home = () => {
 };
 
 export default Home;
+
+
+//optional chaining (?.) negates the need to check if an object even exists
+// before accessing it's properties. in this case no data will exists until
+// the query to the server is finished. so if we type data.thoughts
+// we'll recieve an error saying we can't access the property of data
+// because it is undefined
+// if data exists, store it in the thoughts constant
+// if data is undefined, then save an empty array to the thoughts component
