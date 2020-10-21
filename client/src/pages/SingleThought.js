372c0e958704267677d799fc89c38dfe0cbda709
DJ Viking <anders.swedishviking@gmail.com>
2020-10-21 17:42:15 -0400

    21.4.6 able to render the reaction text on a single thought page

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries.js';
+import ReactionList from '../components/ReactionList.js';
 
 const SingleThought = props => {
   const {
@@ -34,6 +35,12 @@ const SingleThought = props => {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {
+        thought.reactionCount > 0 &&
+        <ReactionList 
+          reactions={thought.reactions}
+        />
+      }
     </div>
   );
 };
